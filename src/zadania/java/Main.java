e572df4a92467feb49da94b2979ab945b7b26927
Marcin Kulpa <mk303205@student.polsl.pl>
2021-12-26 01:05:16 +0100

    zmiana

--- src/zadania/java/Main.java
+++ src/zadania/java/Main.java
@@ -3,6 +3,6 @@ package zadania.java;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Umieść mnie na githubie");
+        System.out.println("Zmiana");
     }
 }
